ccee0d7d1e8b96be945ed47781c080519ca5ffcd
DSMDongly <gdh0608@naver.com>
2017-10-03 14:23:00 +0900

    fixed bug in write poem ajax requestfixed some bug in request body in write poem POST ajax request

--- Renewal/js/WritePoem.js
+++ Renewal/js/WritePoem.js
@@ -18,7 +18,7 @@ function addPoem(title, content, alignment, callback) {
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xhr.setRequestHeader('Poem-Session-Key', localStorage.getItem('Poem-Session-Key'))
 
-    xhr.send(`title=${title}&content=${content}&alignment${alignment}`);
+    xhr.send(`title=${title}&content=${content}&alignment=${alignment}`);
 }
 
 
